dd5043e87947c9414421896d4673b3398f78e67e
Benja <Benja@DESKTOP-PELPGL3>
2024-08-20 18:17:47 -0300

    Add project files.

---
+++ Service/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace Service
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public int StockAvailable { get; set; }
+        public int Edition { get; set; }
+        public string Synopsis { get; set; }
+
+    }
+}
---
+++ Service/Logic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    internal class Logic
+    {
+        public List<Book> books;
+
+        public Logic()
+        {
+            books = new List<Book>();
+        }
+
+
+        public bool AddNewBook(int Id, string Name, int amount, int edition, string synopsis)
+        {
+
+            foreach (Book item in books)
+            {
+                if(item.BookId == Id)
+                {
+                    item.StockAvailable += amount;
+                    return true;
+                }
+            }
+
+
+
+            return true;
+        }
+
+    }
+}
